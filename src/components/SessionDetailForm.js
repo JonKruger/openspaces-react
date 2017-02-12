69c490fa44e682b2815ac65197939f12a27f139a
Jon Kruger <github@jonkruger.com>
2017-02-11 23:14:37 -0500

    form validation

--- src/components/SessionDetailForm.js
+++ src/components/SessionDetailForm.js
@@ -13,38 +13,83 @@ class SessionDetailForm extends React.Component {
     this.props.editSessionDataChanged(e.target.name, e.target.value);
   }
 
-  save() {
+  validateRequired(value) {
+    return value ? true : false;
+  }
+
+  validate(session) {
+    if (session) {
+      this.validFields = {
+        title: this.validateRequired(session.title),
+        owner: this.validateRequired(session.owner)
+      };
+    }
+    else
+      this.validFields = {};
+  }
+
+  isFormValid() {
+    let isValid = true;
+    for (let [value] of Object.entries(this.validFields)) {
+      if (!value)
+        isValid = false;
+    }
+    return isValid;
+  }
+
+  save(e) {
+    e.preventDefault();
     this.props.saveSession(this.props.session);
   }
 
   render() {
-    const session = this.props.session;
+    let session = this.props.session;
     if (!session)
       return <div />;
 
+    this.validate(session);
+
     return (
-    <div>
+    <form onSubmit={this.save}>
       <h1>Edit Session</h1>
 
       <div className="field">
         <label htmlFor="session_title">Title</label><br/>
-          <input required="required" maxLength="75" size="20" type="text" value={session.title} name="title" id="session_title" onChange={this.onFieldChange} />
+          <input 
+            required="required" 
+            maxLength="75" 
+            size="20" 
+            type="text" 
+            value={session.title} 
+            name="title" 
+            id="session_title" 
+            onChange={this.onFieldChange} 
+            className={this.validFields.title ? "" : "invalid"}/>
       </div>
       <div className="field">
         <label htmlFor="session_owner">Your Name</label><br/>
-          <input required="required" maxLength="25" size="20" type="text" value={session.owner} name="owner" id="session_owner" onChange={this.onFieldChange} />
+          <input 
+            required="required" 
+            maxLength="25" 
+            size="20" 
+            type="text" 
+            value={session.owner} 
+            name="owner" 
+            id="session_owner" 
+            onChange={this.onFieldChange}
+            className={this.validFields.owner ? "" : "invalid"}/>
       </div>
       <div className="field">
         <label htmlFor="session_twitterHandle">Twitter handle</label><br/>
           <input size="20" type="text" value={session.twitterHandle} name="twitterHandle" id="session_twitterHandle" onChange={this.onFieldChange} />
       </div>
-
+      <br />
       <div className="actions">
-        <input type="submit" name="commit" value="Save" onClick={this.save} />
+        <input type="submit" name="commit" value="Save" disabled={!this.isFormValid(session)} />
       </div>
       <a data-confirm="Are you sure?" rel="nofollow" data-method="delete" href="/sessions/221">Delete This Session</a> |
       <Link to={"/"}>Back</Link>
-    </div>);
+    </form>);
   }
 }
 
